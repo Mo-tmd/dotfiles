1bfb388d61ce354bdd2660baa56bb78cb5777627
Mo-TMD <>
2024-06-28 02:09:37 +0200

    Fix some bugs in setup_symlinks.sh

--- scripts/setup_symlinks.sh
+++ scripts/setup_symlinks.sh
@@ -19,13 +19,13 @@ CreateSymlink() {
 }
 
 DotFilesDir=$1
-[[ -e "${DotFilesDir}" ]] || echo "Error, \"${DotFilesDir}\" doesn't exist"; exit 0
+[[ -e "${DotFilesDir}" ]] || { echo "Error, \"${DotFilesDir}\" doesn't exist"; exit 0; }
 
 [[ -e ~/.cshrc.user ]] && CshRcFile=~/.cshrc.user || CshRcFile=~/.cshrc
-CreateSymlink CshRcFile "${DotFilesDir}/shell/tcsh/rc.csh"
+CreateSymlink $CshRcFile "${DotFilesDir}/shell/tcsh/rc.csh"
 
 [[ -e ~/.zshrc.user ]] && ZshRcFile=~/.zshrc.user || ZshRcFile=~/.zshrc
-CreateSymlink ZshRcFile "${DotFilesDir}/shell/zsh/rc.sh"
+CreateSymlink $ZshRcFile "${DotFilesDir}/shell/zsh/rc.sh"
 
 CreateSymlink ~/.zshenv "${DotFilesDir}/shell/zsh/.zshenv"
 CreateSymlink ~/.config/nvim "${DotFilesDir}/nvim"
