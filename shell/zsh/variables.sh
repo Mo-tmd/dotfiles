a2c92f1d5fc7e83f66bba82935dbb6fc60749a41
Mo-TMD <>
2024-07-03 14:32:37 +0200

    Source work aliases and variables if they exist
    Change-Id: I073d323040c6a3a052c5dd121b450d2ff1a71ff6

--- shell/zsh/variables.sh
+++ shell/zsh/variables.sh
@@ -1,16 +1,36 @@
-## Source common variables
+################################################################################
+## Source variables that are common between csh and sh
+################################################################################
+## First, put personal and work variables in the same tmp file.
 mkdir -p ~/dump
-File=~/dump/sh_variables_tmp_`uuidgen`
-sed 's/{env_var}/export/g' "${DotFiles}/shell/variables" | \
-sed 's/{separator}/=/g'                                  | \
-sed 's/{shell_var}//g'                                     \
-> $File
-source $File
-
-rm $File
-unset File
-
-## sh specific
-if [[ ! -n "${TBOX_SESSION}" ]]; then
-    export KUBECONFIG=$MY_KUBECONFIG
+TmpCommonVariablesFile=~/dump/common_variables_`uuidgen`
+cp "${DotFiles}/shell/variables" $TmpCommonVariablesFile
+WorkVariables="${WorkDotFiles}/shell/variables"
+if [[ -e "${WorkVariables}" ]]; then
+    cat "${WorkVariables}" >> $TmpCommonVariablesFile
 fi
+unset WorkVariables
+
+## Then convert to sh syntax and source.
+TmpShVariablesFile=~/dump/sh_variables_tmp_`uuidgen`
+sed 's/{env_var}/export/g' "${TmpCommonVariablesFile}" | \
+sed 's/{separator}/=/g'                                | \
+sed 's/{shell_var}//g'                                   \
+> $TmpShVariablesFile
+source $TmpShVariablesFile
+
+## Cleanup
+rm $TmpCommonVariablesFile
+unset TmpCommonVariablesFile
+rm $TmpShVariablesFile
+unset TmpShVariablesFile
+
+################################################################################
+## sh specific
+################################################################################
+WorkShVariables="${WorkDotFiles}/shell/zsh/variables.sh"
+if [[ -e "${WorkShVariables}" ]]; then
+    source "${WorkShVariables}"
+fi
+unset WorkShVariables
+
