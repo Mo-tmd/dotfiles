e8c57b04ce3e9bd6d5abd64a1276f4147719ba77
Mo-TMD <>
2024-07-15 02:53:50 +0200

    Bugfix in tmux_new_saved_session.py
    Handle missing --session-name

--- scripts/tmux/tmux_new_saved_session.py
+++ scripts/tmux/tmux_new_saved_session.py
@@ -4,15 +4,18 @@ from session import Window, Session
 import session_db
 import tmux_new_session
 import subprocess
+import uuid
 
 def main():
     session = tmux_new_session.parse_args()
+    if session.name is None:
+        session.name = str(uuid.uuid4())
     tmux_new_session.new_session(session)
     session_db.store(session)
     cmd("tmux attach-session -d")
 
 def cmd(cmd):
-    output =  subprocess.check_output(cmd, shell=True, text=True).rstrip("\n")
+    subprocess.check_output(cmd, shell=True, text=True).rstrip("\n")
 
 ## Python ugliness.
 if __name__ == "__main__":
